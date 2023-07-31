00751bca82331414bb2f568e6f6a0c2d57449479
Je-chan <jkyyc3@gmail.com>
2023-08-01 00:06:30 +0900

    FEAT : 버블 정렬

---
+++ 00.코드 모음집/정렬 알고리즘/BubbleSort.js
@@ -0,0 +1,12 @@
+const bubbleSort = (arr) => {
+  for (let i = arr.length - 1; i > 0; i--) {
+    for (let j = 0; j < i; j++) {
+      if (arr[j] < arr[j + 1]) {
+        // Swap
+        const temp = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = temp;
+      }
+    }
+  }
+};
