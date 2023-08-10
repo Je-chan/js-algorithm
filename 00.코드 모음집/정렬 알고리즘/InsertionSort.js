f7a05a4e76b44c01efa84eec639989fedab98f6a
Je-chan <jkyyc3@gmail.com>
2023-08-10 22:39:17 +0900

    FEAT : 삽입 정렬

---
+++ 00.코드 모음집/정렬 알고리즘/InsertionSort.js
@@ -0,0 +1,15 @@
+const insertionSort = (arr) => {
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = i; j > 0; j--) {
+      // 인덱스 j부터 1까지 1씩 감소하며 반복
+      if (arr[j] < arr[j - 1]) {
+        const temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+      } else {
+        // 자기보다 작은 데이터를 만나면 그 위치에서 멈춤
+        break;
+      }
+    }
+  }
+};
